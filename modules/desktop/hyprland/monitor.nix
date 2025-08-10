395083633de9f03a799d32238f14f48d2851eb97
fm39hz-laptop <hitpoint2k3@gmail.com>
2025-08-10 15:31:13 +0700

    chore(hyprland): comment out unused monitor config

--- modules/desktop/hyprland/monitor.nix
+++ modules/desktop/hyprland/monitor.nix
@@ -4,7 +4,7 @@
     monitor = [
       "eDP-1, 1920x1080@60, 0x0, 1" # laptop screen
       "DP-1, 1920x1080@170, 0x0, 1, bitdepth, 10" # high refresh rate monitor
-      "HDMI-A-1, 1920x1080@60, 1920x0, 1" # secondary monitor positioned to the right
+      # "HDMI-A-1, 1920x1080@60, 1920x0, 1" # secondary monitor positioned to the right
       ", preferred, auto, 1" # fallback for any other monitors
     ];
     
